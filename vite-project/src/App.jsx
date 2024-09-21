8b8e43d49f3b4d57596fa9b83f2f8d0731987202
hoang cuong <vancuong25112003@gmail.com>
2024-09-22 03:28:02 +0700

    push

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./Components/Navbar/NavBar"
+
+
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,33 @@
+import './NavBar.css'
+import menu_icon from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search.png'
+import upload_icon from '../../assets/upload.png'
+import more_icon from '../../assets/more.png'
+import notification_icon from '../../assets/notification.png'
+import profile_icon from '../../assets/jack.png'
+
+const NavBar = () => {
+    return (
+        <nav className='flex-div'>
+            <div className="nav-left flex-div">
+                <img src={menu_icon} className='menu-icon' alt="" />
+                <img src={logo} className='logo' alt="" />
+            </div>
+            <div className="nav-middle flex-div">
+                <div className="search-box flex-div" >
+                    <input type='text' placeholder='Search'></input>
+                    <img src={search_icon} alt="" />
+                </div>
+            </div>
+            <div className="nav-right">
+                <img src={upload_icon} alt="" />
+                <img src={more_icon} alt="" />
+                <img src={notification_icon} alt="" />
+                <img src={profile_icon} className='user-icon' alt="" />
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
